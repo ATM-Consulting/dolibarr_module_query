cae3f7e2aab331de00acf7722d9b3f5fb0386456
Alexis Algoud <alexis@atm-consulting.fr>
2015-11-27 12:43:46 +0100

    Add gridster

--- dashboard.php
+++ dashboard.php
@@ -117,27 +117,22 @@ function fiche(&$query) {
 		          }
 		    });
 		
+			var gridster = $(".gridster ul").gridster().data('gridster');
+		
+			$('#addQuery').click(function() {
+				gridster.add_widget('<li>New query</li>');
+
+			});
+		
 		});
 			
 	</script>
+	<a href="#" class="butAction" id="addQuery">Ajouter une requête</a>
+	
 	<div class="gridster">
 	    <ul>
 	        <li data-row="1" data-col="1" data-sizex="1" data-sizey="1"></li>
-	        <li data-row="2" data-col="1" data-sizex="1" data-sizey="1"></li>
-	        <li data-row="3" data-col="1" data-sizex="1" data-sizey="1"></li>
 	
-	        <li data-row="1" data-col="2" data-sizex="2" data-sizey="1"></li>
-	        <li data-row="2" data-col="2" data-sizex="2" data-sizey="2"></li>
-	
-	        <li data-row="1" data-col="4" data-sizex="1" data-sizey="1"></li>
-	        <li data-row="2" data-col="4" data-sizex="2" data-sizey="1"></li>
-	        <li data-row="3" data-col="4" data-sizex="1" data-sizey="1"></li>
-	
-	        <li data-row="1" data-col="5" data-sizex="1" data-sizey="1"></li>
-	        <li data-row="3" data-col="5" data-sizex="1" data-sizey="1"></li>
-	
-	        <li data-row="1" data-col="6" data-sizex="1" data-sizey="1"></li>
-	        <li data-row="2" data-col="6" data-sizex="1" data-sizey="2"></li>
 	    </ul>
 	</div>
 	

b77f8eb944f7adddd8dd5f210568a8809b760201
Alexis Algoud <alexis@atm-consulting.fr>
2016-01-22 16:58:00 +0100

    FIX no param for adminer

--- bdd.php
+++ bdd.php
@@ -10,12 +10,14 @@ if(empty($user->rights->query->bdd->write)) {
 
 		$url = 'lib/adminer/?';
 		
+		//TODO genrate read profile
+		/*
 		$url.='&server='.$dolibarr_main_db_host;
 		$url.='&db='.$dolibarr_main_db_name;
 		$url.='&username='.$dolibarr_main_db_user;
 		$url.='&password='.$dolibarr_main_db_pass;
 		$url.='&driver='.$dolibarr_main_db_type;
-
+		*/
 ?>
 
 	<a href="<?php echo $url; ?>" class="butAction" target="_blank">Accès à la base de données</a>

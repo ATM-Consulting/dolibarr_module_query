026e0a79bb963c8f10e99469fcba79e8d69208c4
Alexis Algoud <alexis@atm-consulting.fr>
2016-06-15 11:11:11 +0200

    del /**/

--- mail-dashboard.php
+++ mail-dashboard.php
@@ -65,31 +65,3 @@
 	}
 	fclose($f1);
 	
-	
-	
-	//$temp_file = tempnam(sys_get_temp_dir(), 'QRY_');
-	/*
-	try {
-        $wkhtmltopdf = new Wkhtmltopdf(array('path' => sys_get_temp_dir()));
-        $wkhtmltopdf->setTitle("Query");
-        $wkhtmltopdf->setUrl($url);
-        $wkhtmltopdf->output(Wkhtmltopdf::MODE_SAVE, "file.pdf");
-    } catch (Exception $e) {
-        echo $e->getMessage();
-    }*/
-/*
-	echo (int)dol_include_once('/query/lib/vendor/autoload.php');
-	
-
-	use mikehaertl\wkhtmlto\Pdf;
-
-	
-	$pdf = new Pdf();
-	
-	// On some systems you may have to set the binary path.
-	// $pdf->binary = 'C:\...';
-	print $temp_file;
-	if (!$pdf->saveAs($temp_file)) {
-	    echo $pdf->getError();
-	}
-*/

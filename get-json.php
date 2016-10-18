bf5aee9f3f3c9a7f8672996bacea96bd75d3ff0d
Alexis Algoud <alexis@atm-consulting.fr>
2016-10-18 16:41:00 +0200

    NEW export as JSON query with parameter FIX load return

---
+++ get-json.php
@@ -0,0 +1,26 @@
+<?php
+
+	define('INC_FROM_CRON_SCRIPT',true);
+	
+	require 'config.php';
+	
+	set_time_limit(0);
+	ini_set('memory_limit','128M');
+	
+	dol_include_once('/query/class/query.class.php');
+	
+	$PDOdb=new TPDOdb;
+	
+	$query=new TQuery;
+	if($query->loadBy($PDOdb, GETPOST('uid'),'uid' )) {
+		
+		
+		$query->type='RAW';
+		$Tab =  $query->run($PDOdb);
+
+		__out($Tab['champs'],'json');
+	
+	}
+	else{
+		exit('InvalidUID');
+	}
